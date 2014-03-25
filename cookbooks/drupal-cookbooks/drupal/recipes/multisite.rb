0f0f6dabc13539c5bd7eef1daa6db825dbd843b2
btopro <bto108@psu.edu>
2014-03-25 12:19:47 -0400

    host file creation better

--- cookbooks/drupal-cookbooks/drupal/recipes/multisite.rb
+++ cookbooks/drupal-cookbooks/drupal/recipes/multisite.rb
@@ -10,7 +10,7 @@ if node[:hosts].has_key(:localhost_aliases)
       template "sites.conf.erb"
       server_name site + ".nittany.local"
       server_aliases [site + ".nittany.local"]
-      docroot "#{node[:www_root]}" + '/' + site
+      docroot "#{node[:www_root]}"
     end
   end
 end
