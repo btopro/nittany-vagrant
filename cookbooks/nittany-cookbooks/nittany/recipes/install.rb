44b5d7603243a7ad5dc6c77119b25571748a3759
btopro <bto108@psu.edu>
2014-03-25 12:25:14 -0400

    installer cleaned up and UX on start better

--- cookbooks/nittany-cookbooks/nittany/recipes/install.rb
+++ cookbooks/nittany-cookbooks/nittany/recipes/install.rb
@@ -9,18 +9,20 @@ case node[:platform]
 when "debian", "ubuntu"
   bash "install-nittany-script" do
     code <<-EOH
-    (
-    cd /var/www/nittany
+    (cd /var/www/nittany
     drush site-install nittany -y --db-url=mysql://root:n3I7z0HAYV689Z2541mF@localhost/nittany_dev --db-su=root --db-su-pw=n3I7z0HAYV689Z2541mF  --account-mail="admin@dev.nittany.local" --site-mail="admin@dev.nittany.local" --site-name="Nittany"
     #modify ownership of these directories
-    chown -R www-data:www-data /var/www/nittany/default/files
-    chmod -R 755 /var/www/nittany/default/files
+    chown -R www-data:www-data /var/www/nittany/sites/default/files
+    chmod -R 755 /var/www/nittany/sites/default/files
     # setup private file directory
-    mkdir -p /var/www/nittany_priv
-    chown -R www-data:www-data /var/www/nittany_priv
-    chmod -R 755 /var/www/nittany_priv
+    mkdir -p /var/nittany_priv
+    chown -R www-data:www-data /var/nittany_priv
+    chmod -R 755 /var/nittany_priv
     drush -y --uri=http://dev.nittany.local upwd admin --password=admin
-    )
+    drush -y --uri=http://dev.nittany.local vset file_private_path "/var/nittany_priv"
+    drush -y --uri=http://dev.nittany.local vset site_frontpage "user/login")
+    drush dl drush_ecl registry_rebuild --y
+    drush cc drush
     EOH
     only_if { File.exists?("/var/www/nittany/index.php") }
     not_if { File.exists?("/var/www/nittany/sites/default/settings.php") }
